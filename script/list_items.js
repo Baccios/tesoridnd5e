644e9e8d87f792f5b45fc72a65cdd3bfe9ab8d68
Baccios <baccio.leonardo@gmail.com>
2022-06-27 15:08:12 +0200

    Added a new page with a list of all magic items

---
+++ script/list_items.js
@@ -0,0 +1,102 @@
+let magic_items = null;
+
+function fetchJSONFile(path, callback) {
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                let data = JSON.parse(httpRequest.responseText);
+                if (callback) callback(data);
+            }
+        }
+    };
+    httpRequest.open('GET', path);
+    httpRequest.send();
+}
+
+function load_and_display_items() {
+    fetchJSONFile('./script/json/magic_items.json', function (data) {
+        magic_items = data;
+        onload_display();
+    });
+}
+
+function get_icon_name(category) {
+    switch(category) {
+            case 'Pozione':
+                return './assets/potion_icon.png';
+
+            case 'Oggetto Meraviglioso':
+                return './assets/wondrous_icon.png';
+
+            case 'Scudo':
+                return './assets/shield_icon.png';
+                
+            case 'Arma':
+                return './assets/weapon_icon.png';
+                
+            case 'Armatura':
+                return './assets/armor_icon.png';
+                
+            case 'Anello':
+                return './assets/ring_icon.png';
+                
+            case 'Bacchetta':
+                return './assets/wand_icon.png';
+                
+            case 'Bastone':
+                return './assets/staff_icon.png';
+                
+            case 'Pergamena':
+                return './assets/scroll_icon.png';
+                
+            case 'Verga':
+                return './assets/rod_icon.png';
+                
+        }
+}
+
+function onload_display() {
+    if (magic_items === null) {
+        return;
+    }
+
+    let items_list = document.getElementById("items_list");
+    for (let i = 0; i < magic_items.length; i++) {
+        // add a li to the list with a div with an image and a span with the item name as a link
+        let li = document.createElement("li");
+        let div = document.createElement("div");
+        let img = document.createElement("img");
+        let a = document.createElement("a");
+        img.src = get_icon_name(magic_items[i]['category']);
+        img.alt = magic_items[i]['category']
+        a.href = "./magic_item.html?id=" + magic_items[i]['id'];
+        a.target = "_blank";
+        a.innerHTML = magic_items[i]['name'];
+        div.appendChild(img);
+        div.appendChild(a);
+        li.appendChild(div);
+        li.className = "item_li";
+        items_list.appendChild(li);
+    }
+}
+
+function search_bar() {
+  // Declare variables
+  let input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('search_bar');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("items_list");
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
